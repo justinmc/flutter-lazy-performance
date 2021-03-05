48df561038fb429d168e778f7d56370c1760f229
Justin McCandless <jmccandless@google.com>
2021-03-05 13:16:15 -0800

    Using IV.builder (and an example that doesnt)

--- lib/iv_builder_page.dart
+++ lib/iv_builder_page.dart
@@ -1,7 +1,7 @@
-import 'package:vector_math/vector_math_64.dart' show Vector3, Vector4;
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+import 'table_builder.dart';
 
 class IVBuilderPage extends StatefulWidget {
   const IVBuilderPage({ Key key }) : super(key: key);
@@ -68,87 +68,40 @@ class _IVBuilderPageState extends State<IVBuilderPage> {
       body: Center(
         child: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints constraints) {
-            final Vector3 translation = _transformationController.value.getTranslation() * -1;
-            // TODO(justinmc): This should actually use _transformViewport in IV.
-            final Rect viewport = Rect.fromLTWH(
-              translation.x,
-              translation.y,
-              constraints.maxWidth,
-              constraints.maxHeight,
-            );
-            return InteractiveViewer(
+            return InteractiveViewer.builder(
               alignPanAxis: false,
               constrained: false,
               transformationController: _transformationController,
               maxScale: _maxScale,
               minScale: _minScale,
-              child: Builder(
-                builder: (BuildContext context) {
-                  final double scale = _transformationController.value.getMaxScaleOnAxis();
-                  final Color color = Colors.red.withOpacity((scale - _minScale) / _scaleRange);
-                  //return Container(width: 200, height: 200, color: color);
-                  return _TableBuilder(
-                    rowCount: 60,
-                    columnCount: 6,
-                    builder: (BuildContext context, int row, int column) {
-                      if (!_isCellVisible(row, column, viewport)) {
-                        return Container(height: _cellHeight);
-                      }
-                      return Container(
-                        height: _cellHeight,
-                        //color: row % 2 + column % 2 == 1 ? Colors.white : Colors.grey.withOpacity(0.1),
-                        color: color,
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text('$row x $column'),
-                        ),
-                      );
+              builder: (BuildContext context, Rect viewport) {
+                final double scale = _transformationController.value.getMaxScaleOnAxis();
+                final Color color = Colors.red.withOpacity((scale - _minScale) / _scaleRange);
+                return TableBuilder(
+                  rowCount: 60,
+                  columnCount: 6,
+                  cellWidth: _cellWidth,
+                  builder: (BuildContext context, int row, int column) {
+                    if (!_isCellVisible(row, column, viewport)) {
+                      print('justin $row, $column invisible');
+                      return Container(height: _cellHeight);
                     }
-                  );
-                },
-              ),
+                    print('justin $row, $column visible');
+                    return Container(
+                      height: _cellHeight,
+                      color: color,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('$row x $column'),
+                      ),
+                    );
+                  }
+                );
+              },
             );
           },
         ),
       ),
-    );
-  }
-}
-
-typedef _CellBuilder = Widget Function(BuildContext context, int row, int column);
-
-class _TableBuilder extends StatelessWidget {
-  const _TableBuilder({
-    this.rowCount,
-    this.columnCount,
-    this.builder,
-  }) : assert(rowCount != null && rowCount > 0),
-       assert(columnCount != null && columnCount > 0);
-
-  final int rowCount;
-  final int columnCount;
-  final _CellBuilder builder;
-
-  @override
-  Widget build(BuildContext context) {
-    return Table(
-      // ignore: prefer_const_literals_to_create_immutables
-      columnWidths: <int, TableColumnWidth>{
-        for (int column = 0; column < columnCount; column++)
-          column: const FixedColumnWidth(_IVBuilderPageState._cellWidth),
-      },
-      // ignore: prefer_const_literals_to_create_immutables
-      children: <TableRow>[
-        for (int row = 0; row < rowCount; row++)
-          // ignore: prefer_const_constructors
-          TableRow(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: <Widget>[
-              for (int column = 0; column < columnCount; column++)
-                builder(context, row, column),
-            ],
-          ),
-      ],
     );
   }
 }
